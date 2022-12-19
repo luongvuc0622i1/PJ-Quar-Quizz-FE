9bee0fc799e844fa7ea1a71698a51cf6b7b5b426
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-12-19 16:30:34 +0700

    done lock account

--- src/app/manager-pages/result/list-user/list-user.component.ts
+++ src/app/manager-pages/result/list-user/list-user.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {ResultService} from "../../../service/result/result.service";
 import {User} from "../../../model/user";
 import {MatPaginator} from "@angular/material/paginator";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-list-user',
@@ -10,12 +11,14 @@ import {MatPaginator} from "@angular/material/paginator";
 })
 export class ListUserComponent implements OnInit, AfterViewInit {
   users: User[] = [];
+  roles: any;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private resultService: ResultService) { }
 
   ngOnInit(): void {
-      this.getAll();
+    this.roles = localStorage.getItem('RoleSet_Key');
+    this.getAll();
   }
 
   ngAfterViewInit() {
@@ -34,8 +37,54 @@ export class ListUserComponent implements OnInit, AfterViewInit {
   };
 
   getAll() {
-    this.resultService.getAll().subscribe(userList => {
-      this.users = userList;
-    })
+    if(this.roles[7] == 2) {
+      this.resultService.getAllUser().subscribe(userList => {
+        this.users = userList;
+      })
+    } else if(this.roles[7] == 1) {
+      this.resultService.getAllUserManager().subscribe(userList => {
+        this.users = userList;
+      })
+    }
+  }
+
+  lock(id: number) {
+    this.resultService.lock(id).subscribe(() => {
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Account has locked',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }, e => {
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Fail! You must be admin',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    });
+  }
+
+  open(id: number) {
+    this.resultService.open(id).subscribe(() => {
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Account has unlocked',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }, e => {
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Fail! You must be admin',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    });
   }
 }
--- src/app/service/result/result.service.ts
+++ src/app/service/result/result.service.ts
@@ -3,6 +3,7 @@ import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "../../model/user";
+import {Test} from "../../model/test";
 
 const API_URL=`${environment.apiUrl}`
 
@@ -13,7 +14,19 @@ export class ResultService {
 
   constructor(private http:HttpClient) { }
 
-  getAll(): Observable<User[]> {
-    return this.http.get<User[]>(API_URL + '/user/getUserRoles');
+  getAllUser(): Observable<User[]> {
+    return this.http.get<User[]>(API_URL + '/manager/getUserRoles');
+  }
+
+  getAllUserManager(): Observable<User[]> {
+    return this.http.get<User[]>(API_URL + '/admin/getUserManagerRoles');
+  }
+
+  lock(id: number) {
+    return this.http.post(`${API_URL}/admin/lock/${id}`, id);
+  }
+
+  open(id: number) {
+    return this.http.post(`${API_URL}/admin/open/${id}`, id);
   }
 }
