da0cfe2cb88d2a815c47b2d40596edeb657466b1
TuLiP-411 <65971089+TuLiP-411@users.noreply.github.com>
2022-12-19 17:46:31 +0700

    doin landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -10,10 +10,26 @@ export class LandingPageComponent implements OnInit {
 
   constructor(private router: Router) { }
 
-  ngOnInit(): void {
-  }
-
   account() {
     this.router.navigate(['/account']);
+  }
+
+  data : Date = new Date();
+  focus;
+  focus1;
+
+
+  ngOnInit() {
+
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.add('landing-page');
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.add('navbar-transparent');
+  }
+  ngOnDestroy(){
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.remove('landing-page');
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.remove('navbar-transparent');
   }
 }
