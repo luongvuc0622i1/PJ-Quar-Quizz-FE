84083d7b891e4c8988cd7b3d6b719f9f26309ee2
Compass <ndphong165@gmail.com>
2022-12-21 09:10:44 +0700

    done confirm OTP email

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -82,8 +82,8 @@ export class ForgotPasswordComponent implements OnInit {
 
   forgotPassword() {
     console.log(this.email);
-    swal("OTP sending... Waiting a minus, Please!")
-    this.mailService.forgotPassword(this.email).subscribe(data=>{
+    swal("OTP sending... Please Waiting a minus!")
+    this.mailService.forgotPassword(this.email).subscribe(data => {
       console.log(data)
       if(data == true){
         this.completedSend = true;
@@ -96,6 +96,27 @@ export class ForgotPasswordComponent implements OnInit {
       }
     },error =>  {
       swal("Wrong email, Please try again!","","error")
+    })
+  }
+
+  confirmPassword() {
+    console.log(this.otp);
+    console.log(this.email)
+
+    swal("Confirming your OTP... Please Waiting a minus!")
+    this.mailService.confirmPassword(this.otp, this.email).subscribe(data => {
+      console.log(data)
+      if(data == true){
+        this.completedConfirm = true;
+        this.completedDone = true;
+        swal("Confirmed your OTP, Please check your email","","success");
+        localStorage.removeItem("email");
+        localStorage.removeItem("otp");
+      }else{
+        swal("Wrong OTP");
+      }
+    },error =>  {
+      swal("Wrong OTP or expired OTP code", "", "error");
     })
   }
 
